615debd9af4197f9bd91dd3862f372f58641b883
Shadow <shadowtail117@gmail.com>
2021-05-22 13:19:52 -0700

    Initial project

---
+++ Keep Quiet And Nobody Explodes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Keep_Quiet_And_Nobody_Explodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
